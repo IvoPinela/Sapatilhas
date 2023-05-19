bbd877e84ca1165a16ce9b248d68bb877b414ca6
PINELA <pinelaivo10@gmail.com>
2023-05-19 10:17:43 +0100

    Criado URIMartcher para validar os endereços do content Probider

--- app/src/main/java/pt/ipg/sapatilhas/SapatilhaContentProvider.kt
+++ app/src/main/java/pt/ipg/sapatilhas/SapatilhaContentProvider.kt
@@ -2,6 +2,7 @@ package pt.ipg.sapatilhas
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import pt.ipg.sapatilhas.Bd.SapatilhasOpenHelper
@@ -10,33 +11,54 @@ class SapatilhaContentProvider:ContentProvider(){
     private var bdopenHelper: SapatilhasOpenHelper?=null
 
     override fun onCreate(): Boolean {
-        bdopenHelper=SapatilhasOpenHelper(context)
+        bdopenHelper= SapatilhasOpenHelper(context)
+        return true
+        
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
+        uri: Uri,
+        values: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
         p4: String?
     ): Cursor? {
         TODO("Not yet implemented")
     }
 
-    override fun getType(p0: Uri): String? {
+    companion object{
+        private const val AUTORIDADE="pt.ipg.sapatilhas"
+        const val MARCAS="marcas"
+        const val SAPATILHAS="sapatilhas"
+
+        private const val URI_MARCAS=100
+        private const val URI_SAPATILHAS=200
+        fun  uriMatcher()=UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTORIDADE,MARCAS,URI_MARCAS)
+            addURI(AUTORIDADE,SAPATILHAS,URI_SAPATILHAS)
+            /*
+            content://pt.ipg.sapatilhas/marcas
+            
+             */
+        }
+        
+    }
+
+    override fun getType(uri: Uri): String? {
         TODO("Not yet implemented")
     }
 
-    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
         TODO("Not yet implemented")
     }
 
-    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+    override fun delete(uri: Uri, values: String?, selection: Array<out String>?): Int {
         TODO("Not yet implemented")
     }
 
-    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
         TODO("Not yet implemented")
     }
+    
 
 }
