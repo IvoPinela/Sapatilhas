7aa89e1195ed132beb66d4f1f2d53bbe6e906c6d
PINELA <pinelaivo10@gmail.com>
2023-06-06 10:45:44 +0100

    Ultimo Comit Aula

---
+++ app/src/main/java/pt/ipg/sapatilhas/NewSapatilhaFragment2.kt
@@ -0,0 +1,11 @@
+package pt.ipg.sapatilhas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewSapatilhaFragment2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_sapatilha_fragment2)
+    }
+}
