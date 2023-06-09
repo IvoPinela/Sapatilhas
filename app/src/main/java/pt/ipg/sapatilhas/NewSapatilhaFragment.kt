643c361c4c28a828a0033e3625558a57c4d103b1
PINELA <pinelaivo10@gmail.com>
2023-06-09 09:09:33 +0100

    Implementado menu nova sapatilha

--- app/src/main/java/pt/ipg/sapatilhas/NewSapatilhaFragment.kt
+++ app/src/main/java/pt/ipg/sapatilhas/NewSapatilhaFragment.kt
@@ -3,8 +3,10 @@ package pt.ipg.sapatilhas
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import pt.ipg.sapatilhas.databinding.FragmentNewSapatilhaBinding
 
 
@@ -31,11 +33,35 @@ class NewSapatilhaFragment : Fragment() {
 
         val activity=(activity as MainActivity)
         activity.fragment=this
-        activity.idMenuAtual=R.menu.menu_main
+        activity.idMenuAtual=R.menu.menu_guardar_cancelar
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    fun processaOpcaoMenu(item: MenuItem) : Boolean{
+        return  when(item.itemId) {
+            R.id.action_save-> {
+
+                guardar()
+                true
+            }
+            R.id.action_cancele->{
+
+                cancelar()
+                true
+            }
+
+            else-> false
+        }
+    }
+
+    private fun guardar() {
+        TODO("Not yet implemented")
+    }
+
+    private fun cancelar() {
+        findNavController().navigate(R.id.action_newSapatilhaFragment_to_SneakerListFragment)
     }
 }
